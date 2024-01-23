b866cd505331dc66d8191442d8df34590cb51396
UnaryPlus <ombspring@gmail.com>
2024-01-22 20:19:44 -0500

    upgrade to new version of socket.io

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const server = require("http").Server(app)
-const io = require("socket.io").listen(server)
+const io = require("socket.io")(server)
 
 app.use(express.static(__dirname + "/public"))
 
